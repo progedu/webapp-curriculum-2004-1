1baaa58a0efd86e64f065e03a6feab607ef47720
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-04 16:37:18 +0900

    Add Try

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,8 +1,5 @@
 object Main {
 
-  def createString(size: Int): String = {
-    require(size >= 0, "sizeはゼロ以上である必要があります")
-    (for (i <- 0 to size) yield "a").mkString
-  }
+  def createString(size: Int): Try[String] = ???
 
 }
