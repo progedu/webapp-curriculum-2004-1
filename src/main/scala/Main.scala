f330a8d0be2d35f86c2a3804a5499a49a5c251ff
Masahiro <masahiro.infinite77@gmail.com>
2018-12-11 20:32:18 +0900

    関数createStringをTryを使って実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
+import scala.util.Try
 object Main {
 
-  def createString(size: Int): Try[String] = ???
-
+  def createString(size: Int): Try[String] = {
+    if (size < 0) Try(throw new IllegalArgumentException("sizeはゼロ以上である必要があります"))
+    else {
+      val s = (for (i <- 0 to size) yield "a").mkString
+      Try(s)
+    }
+  }
 }
