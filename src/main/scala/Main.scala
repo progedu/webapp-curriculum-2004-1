da7bb969aed7f632d7d3941470ed9968eb1d322c
Matsuda Kazuyuki <mtdkzyk.github@gmail.com>
2020-03-12 20:45:54 +0900

    解答を確認

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,7 +10,9 @@ object Main {
 */
 
   def createString(size: Int): Try[String] = {
-    if (size <= 0) Try(throw new RuntimeException("sizeはゼロ以上である必要があります"))
-    else Try((for (i <- 0 to size - 1) yield "a").mkString)
+    Try {
+      require(size > 0, "sizeはゼロ以上である必要があります")
+      (for (i <- 0 to size - 1) yield "a").mkString
+    }
   }
 }
