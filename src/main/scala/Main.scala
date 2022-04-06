64707bb8debf84fe38950e26b578b41f2e64c466
Kazuki Nitta <nittakdub@gmail.com>
2022-04-06 15:08:05 +0900

    createStringをTryをつかってリファクタリング

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
+import scala.util.Try
+
 object Main {
 
-  def createString(size: Int): Try[String] = ???
+  def createString(size: Int): Try[String] = {
+    if(size >= 0) {
+      Try((for(i <- 1 to size) yield "a").mkString)
+    } else {
+      Try(throw new RuntimeException("sizeはゼロ以上である必要があります"))
+    }
+  }
 
 }
