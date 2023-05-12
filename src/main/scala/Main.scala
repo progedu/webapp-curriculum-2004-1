32826c705c9589ff91432d2e6727f5de11058d0d
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-12 15:09:30 +0900

    処理をTryで囲むように変更

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
-object Main {
+import scala.util.Try
 
-  def createString(size: Int): Try[String] = ???
-
+def createString(size: Int): Try[String] = {
+  Try {
+    require(size >= 0, "sizeはゼロ以上である必要があります")
+    (for (i <- 0 to size) yield "a").mkString
+  }
 }
