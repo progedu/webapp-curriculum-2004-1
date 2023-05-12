56275011daf144f4f132d231515a16dd7c8597a3
hosh294 <132256088+hosh294@users.noreply.github.com>
2023-05-12 15:11:55 +0900

    Tryブロックの追加

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,10 @@
 import scala.util.Try
 
-def createString(size: Int): Try[String] = {
-  Try {
-    require(size >= 0, "sizeはゼロ以上である必要があります")
-    (for (i <- 0 to size) yield "a").mkString
+object Main {
+  def createString(size: Int): Try[String] = {
+    Try {
+      require(size >= 0, "sizeはゼロ以上である必要があります")
+      (for (i <- 0 to size) yield "a").mkString
+    }
   }
 }
